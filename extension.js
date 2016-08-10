76b7a9209c145e36b6e229b9bb582e989b9ce09f
RemyMarathe <remymarathe86@gmail.com>
2016-08-09 18:25:15 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -46,13 +46,13 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
+      botName: "The Watcher",
       language: "english",
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
-      startupVolume: 0, // 0-100
+      startupVolume: 50, // 0-100
       startupEmoji: false, // true or false
       autowoot: true,
       autoskip: false,
